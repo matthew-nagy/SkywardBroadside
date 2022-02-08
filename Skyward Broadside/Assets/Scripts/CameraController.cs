28fb76526d1529456ed23b3f3aa36fcb88e0da6a
joeblob8354 <joeharrison8354@gmail.com>
2022-02-08 18:39:15 +0000

    Fixed borked camera and ship spawns in the right place now

--- Skyward Broadside/Assets/Scripts/CameraController.cs
+++ Skyward Broadside/Assets/Scripts/CameraController.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject shipCam;
+    GameObject thisCam;
 
     private void Start()
     {
-        Instantiate(shipCam);
+        thisCam = Instantiate(shipCam);
+        thisCam.GetComponent<Cinemachine.CinemachineFreeLook>().m_Follow = transform;
+        thisCam.GetComponent<Cinemachine.CinemachineFreeLook>().m_LookAt = transform;
     }
 
     public void disableFreeCam()
     {
-        shipCam.GetComponent<Cinemachine.CinemachineFreeLook>().m_YAxis.m_InputAxisName = "";
-        shipCam.GetComponent<Cinemachine.CinemachineFreeLook>().m_XAxis.m_InputAxisName = "";
+        thisCam.GetComponent<Cinemachine.CinemachineFreeLook>().m_YAxis.m_InputAxisName = "";
+        thisCam.GetComponent<Cinemachine.CinemachineFreeLook>().m_XAxis.m_InputAxisName = "";
     }
 
     public void enableFreeCam()
     {
-        shipCam.GetComponent<Cinemachine.CinemachineFreeLook>().m_YAxis.m_InputAxisName = "Mouse Y";
-        shipCam.GetComponent<Cinemachine.CinemachineFreeLook>().m_XAxis.m_InputAxisName = "Mouse X";
+        thisCam.GetComponent<Cinemachine.CinemachineFreeLook>().m_YAxis.m_InputAxisName = "Mouse Y";
+        thisCam.GetComponent<Cinemachine.CinemachineFreeLook>().m_XAxis.m_InputAxisName = "Mouse X";
     }
 }
